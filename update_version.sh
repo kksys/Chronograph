6b92baa1deb86ce86b6e4f546e0b49723a6e7921
kksys <78661652+kksys@users.noreply.github.com>
2022-09-25 18:04:24 +0900

    :hammer::sparkles: Add the script to update to specified version (#6)

---
+++ update_version.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+usage() {
+  cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] version
+
+Update version
+
+Available options:
+
+   version      Target version
+-h, --help      Print this help and exit
+EOF
+  exit
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
+parse_params() {
+  # default values of variables set from params
+  flag=0
+
+  args=("$@")
+
+  while :; do
+    echo "$1" | grep -E '^(\d+\.\d+\.\d+)$' > /dev/null
+    local _status=$?
+    if [ "$_status" -eq 0 ]; then
+      version="$1"
+      shift
+      continue
+    fi
+
+    case "${1-}" in
+    -h | --help) usage ;;
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
+
+  # check required params and arguments
+  [[ -z "${version}" ]] && die "Missing required parameter: version"
+  [[ ${#args[@]} -eq 0 ]] && die "Missing script arguments"
+
+  return 0
+}
+
+update_podspec_version() {
+  sed -i '' -E 's/(spec\.version += +)\"[0-9\.]+\"/\1\"'${version}'\"/g' ./Chronometer.podspec
+}
+
+update_xcodeproj_version() {
+  sed -i '' -E 's/(MARKETING_VERSION += +)[0-9\.]+;/\1'${version}';/g' ./Chronometer.xcodeproj/project.pbxproj
+}
+
+main() {
+  update_podspec_version
+  update_xcodeproj_version
+}
+
+parse_params $@
+main
